268a687287e6a61fccb0ffd063ec94fab99b55ef
Malina <Malina@DESKTOP-2TB49QS>
2025-12-18 19:44:14 +0200

    Exercitiul 2

--- Controllers/PredictionController.cs
+++ Controllers/PredictionController.cs
@@ -1,33 +1,80 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.ML;
+using Microsoft.EntityFrameworkCore;
+using Cilibia_Malina_Lab4.Data;
+using Cilibia_Malina_Lab4.Models;
 using static Cilibia_Malina_Lab4.PricePredictionModel;
 
 namespace Price_Prediction.Controllers
 {
     public class PredictionController : Controller
     {
-        public IActionResult Price(ModelInput input)
+        private readonly AppDbContext _context;
+
+        public PredictionController(AppDbContext context)
         {
-            // Load the model
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Price()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Price(ModelInput input)
+        {
+            
             MLContext mlContext = new MLContext();
-            // Create predection engine related to the loaded train model
-            ITransformer mlModel =
-           mlContext.Model.Load("PricePredictionModel.mlnet", out var modelInputSchema); 
-            var predEngine = mlContext.Model.CreatePredictionEngine<ModelInput,
-           ModelOutput>(mlModel);
-            // Try model on sample data to predict fair price
+            ITransformer mlModel = mlContext.Model.Load("PricePredictionModel.mlnet", out var modelInputSchema);
+            var predEngine = mlContext.Model.CreatePredictionEngine<ModelInput, ModelOutput>(mlModel);
+
             ModelOutput result = predEngine.Predict(input);
             ViewBag.Price = result.Score;
+
+            var history = new PredictionHistory
+            {
+                PassengerCount = input.Passenger_count,
+                TripDistance = input.Trip_distance,
+                TripTimeInSecs = input.Trip_time_in_secs,
+                PaymentType = input.Payment_type,
+                PredictedPrice = result.Score,
+                CreatedAt = DateTime.Now
+            };
+
+            _context.PredictionHistories.Add(history);
+            await _context.SaveChangesAsync();
+
             return View(input);
         }
 
 
+        [HttpGet]
+        public async Task<IActionResult> History()
+        {
+            var history = await _context.PredictionHistories
+                .OrderByDescending(p => p.CreatedAt)
+                .ToListAsync();
+
+            return View(history);
+        }
+
+
+        [HttpGet]
+        public IActionResult Time()
+        {
+            return View();
+        }
+
+        [HttpPost]
         public IActionResult Time(Cilibia_Malina_Lab4.TimePredictionModel.ModelInput input)
         {
+          
+
             var mlContext = new MLContext();
 
-            ITransformer mlModel = 
-                mlContext.Model.Load("TimePredictionModel.mlnet", out var modelInputSchema);
+            ITransformer mlModel = mlContext.Model.Load("TimePredictionModel.mlnet", out var modelInputSchema);
 
             var predEngine = mlContext.Model.CreatePredictionEngine<Cilibia_Malina_Lab4.TimePredictionModel.ModelInput, Cilibia_Malina_Lab4.TimePredictionModel.ModelOutput>(mlModel);
 
---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Cilibia_Malina_Lab4.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cilibia_Malina_Lab4.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+        {
+        }
+        public DbSet<PredictionHistory> PredictionHistories { get; set; }
+    }
+}
